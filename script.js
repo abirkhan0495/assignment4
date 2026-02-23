a8c7cb3df062fd0a3b398f7d4a330f84ca0256b6
abirkhan0495 <abirkhan0495@gmail.com>
2026-02-24 03:53:47 +0600

    js code

---
+++ script.js
@@ -0,0 +1,11 @@
+let totalCount = document.getElementById("totalCount");
+let interviewCount = document.getElementById("interviewCount");
+let rejectedCount = document.getElementById("rejectedCount");
+
+let sectionJobsCount = document.getElementById("sectionJobsCount");
+let emptyState = document.getElementById("emptyState");
+
+let tabs = document.querySelectorAll("[data-tab]");
+let activeTab = "all";
+
+
